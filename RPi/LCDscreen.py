d2b1abd9fe365e51e2971d6206d41d6aa712763a
yhw2003 <321612813@qq.com>
2022-10-08 07:32:13 -0400

    type

--- RPi/LCDscreen.py
+++ RPi/LCDscreen.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
-import sleep from time
-
+from time import sleep
 GPIO.setmode(GPIO.BOARD)
 wDATA = 1
 wCMD =0
@@ -9,7 +8,7 @@ wCMD =0
 class screen:
     def __init__(self,RWpin,RSpin,Epin,DATApins):
         self.status = 0
-        self.RWpib = RWpin
+        self.RWpin = RWpin
         self.Epin = Epin
         self.RSpin = RSpin
         self.DATApins = DATApins
@@ -21,38 +20,48 @@ class screen:
         for _pin in DATApins:
             GPIO.setup(_pin,GPIO.OUT)
 
-    def write(mode,DATA):
-        i = 0
+    def write(self,mode,DATA):
         GPIO.OUT(self.RWpin,GPIO.LOW)
         if mode == wDATA:
             GPIO.output(self.RSpin,GPIO.LOW)
         if mode == wCMD:
             GPIO.output(self.RSpin,GPIO.HIGH)
-        for tPIN in self.DATApins:
-            if DATA[i] == 1:
-                GPIO.output(tPIN,GPIO.HIGH)
-            if DATA[i] == 0:
-                GPIO.output(tPIN,GPIO.LOW)
-            i = i + 1
-        sleep(0.001)
-        GPIO,output(self.Epin,GPIO.HIGH)
-        sleep(0.001)
-        GPIO.output(self.Epin,GPIO.LOW)
+        for tpin in self.DATApins():
+            GPIO.output(tpin,GPIO.LOW)
+        if DATA&0x10==0x10:
+            GPIO.output(self.DATApins[0],GPIO.HIGH)
+        if DATA&0x20==0x20:
+            GPIO.output(self.DATApins[1],GPIO.HIGH)
+        if DATA&0x40==0x40:
+            GPIO.output(self.DATApins[2],GPIO.HIGH)
+        if DATA&0x80==0x80:
+            GPIO.output(self.DATApin[[3],GPIO.HIGH)
+        
+        self.refresh()
+        if DATA&0x01==0x01:
+            GPIO.output(self.DATApins[0],GPIO.HIGH)
+        if DATA&0x02==0x02:
+            GPIO.output(self.DATApins[1],GPIO.HIGH)
+        if DATA&0x04==0x04:
+            GPIO.output(self.DATApins[2],GPIO.HIGH)
+        if DATA&0x08==0x08:
+            GPIO.output(self.DATApins[0],GPIO.HIGH)
+        self.refresh()
 
-    def init():
+
+    def init(self):
 
 
     def refresh(self):
-        if self.status == 0:
-            GPIO.output(self.Epin,GPIO.HIGH)
-            self.status = 1
-        if self.status == 1:
-            GPIO.output(self.Epin,GPIO.LOW)
-            self.status = 0
+        sleep(0.001)
+        GPIO.output(Epin,GPIO.HIGH)
+        sleep(0.001)
+        GPIO.output(Epin,GPIO.LOW)
+
 
 if __name__ == "__main__":
     DATApins = [35,36,37,38]
-    myScreen = screen(7,40,DATApins)
-    myScreen.refresh()
+    myScreen = screen(7,33,40,DATApins)
+    myScreen.init()
     GPIO.cleanup()
 
